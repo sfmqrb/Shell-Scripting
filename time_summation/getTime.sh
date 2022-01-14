d5fb68c270bd92c5c65cfb6213f9a6a5f593ea4f
Sajad F. Maghrebi <sajad.faghfoor@gmai.com>
2022-01-14 16:39:35 +0330

    time sum

--- time_summation/getTime.sh
+++ time_summation/getTime.sh
@@ -7,8 +7,8 @@ do
    a=$(echo "$f")
    min=$(ffmpeg -i "$f" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,// | cut -d ":" -f2)
    sec=$(ffmpeg -i "$f" 2>&1 | grep Duration | cut -d ' ' -f 4 | sed s/,// | cut -d ":" -f3 | cut -d "." -f1)
-   # echo "$min $sec"
    sum=$((10#$sum+10#$min*60+(10#$sec)))
-   # echo "Total: $sum seconds of video"
 done
-echo "Total: $((sum/3600)) hours of video"
+tHours=$((sum/3600))
+tMinutes=$((sum/60%60))
+echo "Total: $tHours hours $tMinutes minutes of video"
